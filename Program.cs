5181c0885aa592a2007f706828487ba28f8c2284
chitrang <chitrang313@gmail.com>
2023-09-16 15:07:23 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿class Program {
+    delegate void LogDel(string information);
+
+    static void Main(string[] args) {
+        Log log = new Log();
+
+        LogDel logTextToScreenDel = new LogDel(log.LogTextToScreen);
+        LogDel logTextToFileDel = new LogDel(log.LogTextToFile);
+
+        LogDel multiLogDel = logTextToFileDel + logTextToScreenDel;
+
+        Console.WriteLine("Enter your message:");
+
+        string message = Console.ReadLine();
+
+        multiLogDel(message); 
+    }    
+}
+
+class Log {
+    public void LogTextToScreen(string message) {
+        Console.WriteLine($"{DateTime.Now}: {message}");
+    }
+
+    public void LogTextToFile(string message) {
+        string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"LogFile.txt");
+
+        using (StreamWriter sw = new StreamWriter(Path.Combine(filePath),true)) {
+            sw.WriteLine($"{DateTime.Now}: {message}");
+        }
+    }
+}
